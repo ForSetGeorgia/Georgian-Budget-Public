cbf18ea698fdc7c754f4ebe56cc132a60afd38a2
nwshane <django09@gmail.com>
2016-09-24 10:47:11 +0400

    Added Router with Route to Explore component.

--- js/components/App.jsx
+++ js/components/App.jsx
@@ -3,12 +3,16 @@ const React = require('react')
 const { Provider } = require('react-redux')
 const Explore = require('./Explore')
 
+const { Router, Route, hashHistory } = require('react-router')
+
 const store = require('../store')
 
 const App = function () {
   return (
     <Provider store={store}>
-      <Explore />
+      <Router history={hashHistory}>
+        <Route path='/explore' component={Explore} />
+      </Router>
     </Provider>
   )
 }
