dbcc686b49111f9a9fe811cdc00e6ca23a7f06fe
nwshane <django09@gmail.com>
2016-09-26 02:34:58 +0400

    Moved reducers to separate file.

--- js/store.js
+++ js/store.js
@@ -1,46 +1,7 @@
 const { combineReducers, compose, createStore } = require('redux')
+const reducers = require('./reducers')
 
-const error = (state = '', action) => {
-  switch (action.type) {
-    case 'SET_ERROR': {
-      return action.error
-    }
-    case 'CLEAR_ERROR': {
-      return ''
-    }
-    default: {
-      return state
-    }
-  }
-}
-
-const budgetItems = (state = [], action) => {
-  switch (action.type) {
-    case 'SET_BUDGET_ITEMS': {
-      return action.budgetItems
-    }
-    default: {
-      return state
-    }
-  }
-}
-
-const selectedItem = (state = 1, action) => {
-  switch (action.type) {
-    case 'SET_SELECTED_BUDGET_ITEM_ID': {
-      return action.selectedItem
-    }
-    default: {
-      return state
-    }
-  }
-}
-
-const reducer = combineReducers({
-  error,
-  budgetItems,
-  selectedItem
-})
+const reducer = combineReducers(reducers)
 
 const initialState = {
   selectedItem: 1,
