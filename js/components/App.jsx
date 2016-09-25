6b35d0930d46f4dc50131d17b9bf388ae2ea7fd3
nwshane <django09@gmail.com>
2016-09-26 01:59:10 +0400

    Setup server side rendering.

--- js/components/App.jsx
+++ js/components/App.jsx
@@ -3,14 +3,14 @@ const React = require('react')
 const { Provider } = require('react-redux')
 const Routes = require('./Routes')
 
-const { Router, hashHistory } = require('react-router')
+const { Router, browserHistory } = require('react-router')
 
 const store = require('../store')
 
 const App = function () {
   return (
     <Provider store={store}>
-      <Router history={hashHistory}>
+      <Router history={browserHistory}>
         {Routes()}
       </Router>
     </Provider>
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   context: __dirname,
-  entry: './js/main.jsx',
+  entry: './js/browser.jsx',
   output: {
     path: path.join(__dirname, '/public'),
     filename: 'bundle.js'
