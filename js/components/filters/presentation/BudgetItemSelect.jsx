f98e0bca5b9f9aad48762d5b690a58676fbe0d7f
nwshane <django09@gmail.com>
2016-10-03 12:32:53 +0400

    Made budget item select presentation component stateless.

--- js/components/filters/presentation/BudgetItemSelect.jsx
+++ js/components/filters/presentation/BudgetItemSelect.jsx
@@ -1,47 +1,45 @@
 const React = require('react')
-const { func, number, arrayOf, shape, string, bool } = React.PropTypes
-
 const Select = require('react-select')
 
-let BudgetItemSelect = React.createClass({
-  propTypes: {
-    handleChange: func.isRequired,
-    selectedIds: arrayOf(number).isRequired,
-    budgetItemType: string,
-    options: arrayOf(shape({
-      id: number.isRequired,
-      name: string.isRequired
-    })).isRequired,
-    hidden: bool,
-    labelText: string,
-    loading: bool
-  },
+const BudgetItemSelect = (props) => {
+  let style = {}
+  if (props.hidden) style.display = 'none'
 
-  render: function () {
-    let style = {}
-    if (this.props.hidden) style.display = 'none'
+  return (
+    <div className='filter' style={style}>
+      <label htmlFor='budget-item-select'>
+        {props.labelText}
+      </label>
+      <Select
+        id='budget-item-select'
+        name='budget-item-select'
+        value={props.selectedIds}
+        options={props.options}
+        onChange={props.handleChange}
+        disabled={props.options.length === 0}
+        isLoading={props.loading}
+        labelKey='name'
+        valueKey='id'
+        multi
+        simpleValue
+      />
+    </div>
+  )
+}
 
-    return (
-      <div className='filter' style={style}>
-        <label htmlFor='budget-item-select'>
-          {this.props.labelText}
-        </label>
-        <Select
-          id='budget-item-select'
-          name='budget-item-select'
-          value={this.props.selectedIds}
-          options={this.props.options}
-          onChange={this.props.handleChange}
-          disabled={this.props.options.length === 0}
-          isLoading={this.props.loading}
-          labelKey='name'
-          valueKey='id'
-          multi
-          simpleValue
-        />
-      </div>
-    )
-  }
-})
+const { func, number, arrayOf, shape, string, bool } = React.PropTypes
+
+BudgetItemSelect.propTypes = {
+  handleChange: func.isRequired,
+  selectedIds: arrayOf(number).isRequired,
+  budgetItemType: string,
+  options: arrayOf(shape({
+    id: number.isRequired,
+    name: string.isRequired
+  })).isRequired,
+  hidden: bool,
+  labelText: string,
+  loading: bool
+}
 
 module.exports = BudgetItemSelect
