7f2d5be13c294f9acb92c457ed9cb9d86b2e1751
nwshane <django09@gmail.com>
2016-10-03 15:14:53 +0400

    Importing react-select in styles.css instead of with link tag.

--- js/components/Layout.jsx
+++ js/components/Layout.jsx
@@ -12,7 +12,6 @@ const Layout = (props) => {
         title='Home'
         titleTemplate='%s | Georgian Budget'
         link={[
-          {'rel': 'stylesheet', 'href': 'https://unpkg.com/react-select/dist/react-select.css'},
           {'rel': 'stylesheet', 'href': '/public/bundle.css'}
         ]}
       />
