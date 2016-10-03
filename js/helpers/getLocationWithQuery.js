451ece9337d1ac2e141a9ee708fb51132c6d43a4
nwshane <django09@gmail.com>
2016-10-03 13:50:25 +0400

    Only permitting certain query params to be updated in URL.

--- js/helpers/getLocationWithQuery.js
+++ js/helpers/getLocationWithQuery.js
@@ -1,13 +1,25 @@
-module.exports = (location, query) => (
-  Object.assign(
-    {},
-    location,
-    {
-      query: Object.assign(
-        {},
-        location.query,
-        query
-      )
+module.exports = (location, query) => {
+  const newQueryKeys = Object.getOwnPropertyNames(query)
+
+  const allowedQueryKeys = ['financeType', 'budgetItemIds', 'budgetItemType']
+
+  for (let i = 0; i < newQueryKeys.length; i++) {
+    if (!allowedQueryKeys.includes(newQueryKeys[i])) {
+      throw `Query key ${newQueryKeys[i]} is not allowed`
     }
+  }
+
+  return (
+    Object.assign(
+      {},
+      location,
+      {
+        query: Object.assign(
+          {},
+          location.query,
+          query
+        )
+      }
+    )
   )
-)
+}
