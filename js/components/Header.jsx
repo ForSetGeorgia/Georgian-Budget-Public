c19122cf0509a6100bd4963ff96a84442fd1b655
nwshane <django09@gmail.com>
2016-10-06 16:51:16 +0400

    Added max-content-width scss variable and configured webpack to process scss.

--- js/components/Header.jsx
+++ js/components/Header.jsx
@@ -4,20 +4,22 @@ const LocaleLink = require('js/components/header/LocaleLink')
 
 const Header = () => (
   <div className='app-header'>
-    <Link to='/ka/explore'>
-      Explore
-    </Link>
-    <Link to='/ka/about'>
-      შესახებ
-    </Link>
-    <LocaleLink
-      text='ქართული'
-      locale='ka'
-    />
-    <LocaleLink
-      text='English'
-      locale='en'
-    />
+    <header className='app-header-content'>
+      <Link to='/ka/explore'>
+        Explore
+      </Link>
+      <Link to='/ka/about'>
+        შესახებ
+      </Link>
+      <LocaleLink
+        text='ქართული'
+        locale='ka'
+      />
+      <LocaleLink
+        text='English'
+        locale='en'
+      />
+    </header>
   </div>
 )
 
--- webpack.config.js
+++ webpack.config.js
@@ -44,8 +44,13 @@ const loaders = [
   },
   {
     test: /\.scss$/,
-    // loader: 'style!css!sass'
-    loader: ExtractTextPlugin.extract('style-loader', UGLIFY ? 'css-loader?minimize!' : 'css-loader')
+    loader: ExtractTextPlugin.extract(
+      'style',
+      [
+        UGLIFY ? 'css?minimize!' : 'css',
+        'sass'
+      ]
+    )
   }
 ]
 
