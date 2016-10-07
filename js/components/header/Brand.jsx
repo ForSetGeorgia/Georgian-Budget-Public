d6ed6d0f142adade6fe044ec9faa12ce84d9835f
nwshane <django09@gmail.com>
2016-10-07 14:20:17 +0400

    Made Brand into link.

--- js/components/header/Brand.jsx
+++ js/components/header/Brand.jsx
@@ -1,9 +1,20 @@
 const React = require('react')
+const { Link } = require('react-router')
 
-module.exports = () => {
+const Brand = (_props, { currentLocale }) => {
   return (
     <h1 className='brand'>
-      ბიუჯეტის ამბები
+      <Link className='brand-link' to={`/${currentLocale}`}>
+        ბიუჯეტის ამბები
+      </Link>
     </h1>
   )
 }
+
+const { string } = React.PropTypes
+
+Brand.contextTypes = {
+  currentLocale: string
+}
+
+module.exports = Brand
