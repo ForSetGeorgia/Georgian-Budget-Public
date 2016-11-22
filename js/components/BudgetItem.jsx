32dfdfdeef95d4ef07561d60b346387b9a47766d
nwshane <django09@gmail.com>
2016-11-22 18:23:19 +0400

    Refactored BudgetItem to only show time series chart if finances is available.

--- js/components/BudgetItem.jsx
+++ js/components/BudgetItem.jsx
@@ -6,23 +6,31 @@ const TimeSeriesChart = require('js/components/TimeSeriesChart')
 const timePeriodTypeMessages = require('js/messages/timePeriodTypes')
 
 const BudgetItem = (props) => {
-  const timePeriodTypeMessage = props.intl.formatMessage(timePeriodTypeMessages[props.timePeriodType])
-  const timeSeriesChartTitle = `${props.financeType}: ${timePeriodTypeMessage}`
+  const { id, name, financeType, timePeriodType, timePeriods, amounts, intl } = props
+
+  let chart = ''
+
+  if (financeType && timePeriodType && timePeriods && amounts && intl) {
+    const timePeriodTypeMessage = intl.formatMessage(timePeriodTypeMessages[timePeriodType])
+    const timeSeriesChartTitle = `${financeType}: ${timePeriodTypeMessage}`
+
+    chart = <TimeSeriesChart
+      containerId={`${id}-${financeType}-${timePeriodType}-chart`}
+      key={`${id}-${financeType}-${timePeriodType}-chart`}
+      title={timeSeriesChartTitle}
+      xAxisCategories={timePeriods}
+      yAxisAmounts={amounts}
+    />
+  }
 
   return (
     <div className='gb-BudgetItem'>
 
       <h3 className='gb-BudgetItem-heading'>
-        {props.name}
+        {name}
       </h3>
 
-      <TimeSeriesChart
-        containerId={`${props.id}-${props.financeType}-${props.timePeriodType}-chart`}
-        key={`${props.id}-${props.financeType}-${props.timePeriodType}-chart`}
-        title={timeSeriesChartTitle}
-        xAxisCategories={props.timePeriods}
-        yAxisAmounts={props.amounts}
-      />
+      {chart}
 
     </div>
   )
@@ -32,11 +40,11 @@ const { array, string } = React.PropTypes
 
 BudgetItem.propTypes = {
   id: string.isRequired,
-  financeType: string.isRequired,
-  timePeriodType: string.isRequired,
   name: string.isRequired,
-  timePeriods: array.isRequired,
-  amounts: array.isRequired,
+  financeType: string,
+  timePeriodType: string,
+  timePeriods: array,
+  amounts: array,
   intl: intlShape
 }
 
