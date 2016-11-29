96ec27441c986e8b84137229066ea9a6cf5e777e
nwshane <django09@gmail.com>
2016-11-29 16:53:39 +0400

    Updating url with state of time periods filter.

--- js/components/Routes/Explore/components/UrlQueryUpdater.jsx
+++ js/components/Routes/Explore/components/UrlQueryUpdater.jsx
@@ -3,8 +3,12 @@ const { arrayOf, object, string } = React.PropTypes
 const { connect } = require('react-redux')
 
 const { getSelectedBudgetItemIds } = require('js/redux/ducks/explore')
-const { getSelectedBudgetItemType } = require('js/redux/ducks/filters')
-const { getSelectedFinanceType } = require('js/redux/ducks/filters')
+
+const {
+  getSelectedBudgetItemType,
+  getSelectedFinanceType,
+  getSelectedTimePeriods
+ } = require('js/redux/ducks/filters')
 
 const UrlQueryUpdater = React.createClass({
   contextTypes: {
@@ -15,20 +19,26 @@ const UrlQueryUpdater = React.createClass({
   propTypes: {
     budgetItemType: string.isRequired,
     budgetItemIds: arrayOf(string).isRequired,
-    financeType: string.isRequired
+    financeType: string.isRequired,
+    timePeriods: arrayOf(string).isRequired
   },
 
   newQueryObject () {
     const {
       budgetItemIds,
       budgetItemType,
-      financeType
+      financeType,
+      timePeriods
     } = this.props
 
     const newQueryObject = {}
 
     if (budgetItemIds && budgetItemIds.length > 0) {
       newQueryObject.budgetItemIds = budgetItemIds
+    }
+
+    if (timePeriods && timePeriods.length > 0) {
+      newQueryObject.timePeriods = timePeriods
     }
 
     if (budgetItemType) newQueryObject.budgetItemType = budgetItemType
@@ -72,7 +82,8 @@ const UrlQueryUpdater = React.createClass({
 const mapStateToProps = (state) => ({
   budgetItemType: getSelectedBudgetItemType(state),
   budgetItemIds: getSelectedBudgetItemIds(state),
-  financeType: getSelectedFinanceType(state)
+  financeType: getSelectedFinanceType(state),
+  timePeriods: getSelectedTimePeriods(state)
 })
 
 module.exports = connect(mapStateToProps)(UrlQueryUpdater)
