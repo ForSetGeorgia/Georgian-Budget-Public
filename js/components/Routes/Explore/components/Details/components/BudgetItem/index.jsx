8a5fdfa675faeb1dfaf83531a28a5ef26bc3501a
nwshane <django09@gmail.com>
2016-11-30 16:55:12 +0400

    Made it possible to select time period for filtering details charts.

--- js/components/Routes/Explore/components/Details/components/BudgetItem/index.jsx
+++ js/components/Routes/Explore/components/Details/components/BudgetItem/index.jsx
@@ -1,19 +1,19 @@
 const React = require('react')
-const { arrayOf, func, number, string } = React.PropTypes
+const { arrayOf, func, string } = React.PropTypes
 const { connect } = require('react-redux')
 
 const FinancesTimeSeries = require('./components/FinancesTimeSeries')
 const LoadingIndicator = require('js/components/shared/LoadingIndicator')
 
 const fetchBudgetItemDetails = require('js/redux/fetchers/fetchBudgetItemDetails')
+const { getSelectedTimePeriods } = require('js/redux/ducks/filters')
 
 const BudgetItem = React.createClass({
   propTypes: {
     id: string.isRequired,
     name: string.isRequired,
     loaded: arrayOf(string).isRequired,
-    spentFinances: arrayOf(number),
-    plannedFinances: arrayOf(number),
+    selectedTimePeriod: string,
     fetchBudgetItemDetails: func
   },
 
@@ -27,17 +27,20 @@ const BudgetItem = React.createClass({
     this.props.fetchBudgetItemDetails(this.props.id)
   },
 
-  renderChart (timePeriodType, options) {
+  renderChart (timePeriodType) {
     if (!this.detailsLoaded()) return
-    const { id } = this.props
+    const { id, selectedTimePeriod } = this.props
+
+    const surroundingTimePeriod = timePeriodType === 'year' ? 'all' : selectedTimePeriod
 
     return (
       <FinancesTimeSeries
+        key={`${timePeriodType}-${selectedTimePeriod}`}
         itemIds={[id]}
         timePeriodType={timePeriodType}
         showSpentFinances
         showPlannedFinances
-        {...options}
+        inTimePeriod={surroundingTimePeriod}
       />
     )
   },
@@ -46,8 +49,8 @@ const BudgetItem = React.createClass({
     return (
       <div>
         {this.renderChart('year')}
-        {this.renderChart('quarter', { inTimePeriod: 'y2014' })}
-        {this.renderChart('month', { inTimePeriod: 'y2014' })}
+        {this.renderChart('quarter')}
+        {this.renderChart('month')}
       </div>
     )
   },
@@ -77,10 +80,14 @@ const BudgetItem = React.createClass({
   }
 })
 
+const mapStateToProps = state => ({
+  selectedTimePeriod: getSelectedTimePeriods(state)[0]
+})
+
 const mapDispatchToProps = dispatch => ({
   fetchBudgetItemDetails: itemId => {
     dispatch(fetchBudgetItemDetails(itemId))
   }
 })
 
-module.exports = connect(null, mapDispatchToProps)(BudgetItem)
+module.exports = connect(mapStateToProps, mapDispatchToProps)(BudgetItem)
