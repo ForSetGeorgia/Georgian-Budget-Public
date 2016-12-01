d1c93f86342706e8a24b1b1ef30708170d757460
nwshane <django09@gmail.com>
2016-12-01 17:05:24 +0400

    Only showing selected time period.

--- js/components/Routes/Explore/components/Details/components/BudgetItem/components/BudgetItemYearlyTable.jsx
+++ js/components/Routes/Explore/components/Details/components/BudgetItem/components/BudgetItemYearlyTable.jsx
@@ -6,15 +6,24 @@ const { getYearsWithData } = require('js/redux/modules/year')
 const { getItemSpentFinances } = require('js/redux/entities/budgetItem')
 const { getItemPlannedFinances } = require('js/redux/entities/budgetItem')
 const { translateTimePeriod } = require('js/redux/entities/timePeriod')
+const { getSelectedTimePeriods } = require('js/redux/ducks/filters')
 
 const getColumns = () => (
   ['year', 'spent_finance', 'planned_finance', 'difference']
 )
 
+const maxTwoDecimal = num => {
+  if (Math.round(num) === num) {
+    return num
+  } else {
+    return num.toFixed(2)
+  }
+}
+
 const getDifference = (finance1, finance2) => {
   if (!finance1 || !finance2) return ''
 
-  return finance1.amount - finance2.amount
+  return maxTwoDecimal(finance1.amount - finance2.amount)
 }
 
 const getDataForYear = (year, spentFinances, plannedFinances) => {
@@ -29,11 +38,20 @@ const getDataForYear = (year, spentFinances, plannedFinances) => {
   }
 }
 
+const getVisibleYears = (state) => {
+  const years = getYearsWithData()
+  const selectedTimePeriod = getSelectedTimePeriods(state)[0]
+
+  if (selectedTimePeriod === 'all') return years
+
+  return years.filter(year => year === selectedTimePeriod)
+}
+
 const getResults = (state, itemId) => {
   const spentFinances = getItemSpentFinances(state, itemId)
   const plannedFinances = getItemPlannedFinances(state, itemId)
 
-  return getYearsWithData().map(year => (
+  return getVisibleYears(state).map(year => (
     getDataForYear(year, spentFinances, plannedFinances)
   ))
 }
