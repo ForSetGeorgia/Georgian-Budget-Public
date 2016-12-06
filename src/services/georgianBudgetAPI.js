03bb6b3ea8bffa98b0baa26239f6c712346a4641
nwshane <django09@gmail.com>
2016-12-06 11:07:56 +0400

    Commented out headers in axios.

--- src/services/georgianBudgetAPI.js
+++ src/services/georgianBudgetAPI.js
@@ -4,8 +4,8 @@ const { addError } = require('src/data/ducks/errors')
 const { dispatch } = require('src/data/store')
 
 function get (locale, version, options) {
-  axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
-  axios.defaults.headers.common['X-Key-Inflection'] = 'camel'
+  // axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+  // axios.defaults.headers.common['X-Key-Inflection'] = 'camel'
   return axios.get(
     `${process.env.API_URL}/${locale}/${version}`,
     {
