e216fb2618db30ae97a8331345a569ffda3cc5ff
nwshane <django09@gmail.com>
2016-12-06 11:42:49 +0400

    Uncommented old headers code in georgianBudgetAPI.

--- src/services/georgianBudgetAPI.js
+++ src/services/georgianBudgetAPI.js
@@ -4,20 +4,14 @@ const { addError } = require('src/data/ducks/errors')
 const { dispatch } = require('src/data/store')
 
 function get (locale, version, options) {
-  axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
-  axios.defaults.headers.common['X-Key-Inflection'] = 'camel'
   return axios.get(
     `${process.env.API_URL}/${locale}/${version}`,
     {
-      params: options.params
-      // headers: Object.assign(
-      //   {},
-      //   axios.defaults.headers.common,
-      //   {
-      //     'X-Requested-With': 'XMLHttpRequest',
-      //     'X-Key-Inflection': 'camel'
-      //   }
-      // )
+      params: options.params,
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-Key-Inflection': 'camel'
+      }
     }
   ).catch((error) => {
     dispatch(addError(`Error communicating with API: ${error}`))
