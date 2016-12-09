06011e8ca86a8432d4c40fc37a373bb8828c5bb6
nwshane <django09@gmail.com>
2016-12-09 15:49:38 +0400

    Code style fix.

--- src/components/Routes/Explore/components/List/components/CountDisplay.jsx
+++ src/components/Routes/Explore/components/List/components/CountDisplay.jsx
@@ -2,23 +2,21 @@ const React = require('react')
 const { number, object } = React.PropTypes
 const { FormattedMessage, injectIntl, intlShape } = require('react-intl')
 
-const CountDisplay = ({ count, itemTranslations, intl }) => {
-  return (
-    <FormattedMessage
-      id='app.explore.list.count'
-      description='Displays the number of items in the explore list'
-      defaultMessage='Showing {count} {count, plural,
-        one {{one}}
-        other {{other}}
-      }.'
-      values={{
-        count: count,
-        one: intl.formatMessage(itemTranslations.one),
-        other: intl.formatMessage(itemTranslations.afterNumber)
-      }}
-    />
-  )
-}
+const CountDisplay = ({ count, itemTranslations, intl }) => (
+  <FormattedMessage
+    id='app.explore.list.count'
+    description='Displays the number of items in the explore list'
+    defaultMessage='Showing {count} {count, plural,
+      one {{one}}
+      other {{other}}
+    }.'
+    values={{
+      count: count,
+      one: intl.formatMessage(itemTranslations.one),
+      other: intl.formatMessage(itemTranslations.afterNumber)
+    }}
+  />
+)
 
 CountDisplay.propTypes = {
   count: number,
