fb46ec241ece55884a7c3b25fd57a068c80f12fa
nwshane <django09@gmail.com>
2016-12-27 17:05:40 +0400

    Improved colors and size of LoadingIndicator.

--- src/components/shared/LoadingIndicator.jsx
+++ src/components/shared/LoadingIndicator.jsx
@@ -2,6 +2,27 @@ const React = require('react')
 const Svg = require('src/components/shared/Svg')
 const svg = require('public/images/loading_indicator')
 
-const LoadingIndicator = () => <Svg markup={svg} />
+const outerContainerStyle = {
+  display: 'flex',
+  'justifyContent': 'center'
+}
+
+const size = '600px'
+
+const innerContainerStyle = {
+  display: 'block',
+  'width': size,
+  'height': size,
+  'maxWidth': '100%',
+  'maxHeight': '100%'
+}
+
+const LoadingIndicator = () => (
+  <span style={outerContainerStyle}>
+    <span style={innerContainerStyle}>
+      <Svg markup={svg} />
+    </span>
+  </span>
+)
 
 module.exports = LoadingIndicator
