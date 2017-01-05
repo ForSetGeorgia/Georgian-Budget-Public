e7bf8dc9f90a77ca8e7f87e761a1636d7a27a8da
nwshane <django09@gmail.com>
2017-01-06 01:34:06 +0400

    Replaced default griddle styling with custom table styling.

--- src/components/shared/CustomGriddle.jsx
+++ src/components/shared/CustomGriddle.jsx
@@ -3,7 +3,9 @@ const Griddle = require('griddle-react')
 
 const CustomGriddle = props => (
   <Griddle
+    tableClassName='gb-Table'
     rowMetadata={{ bodyCssClassName: 'gb-Table-row mod-darker-if-odd' }}
+    useGriddleStyles={false}
     {...props}
   />
 )
