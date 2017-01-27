a22313a6872affd870654a5a9f81a8774be4a95b
nwshane <django09@gmail.com>
2017-01-27 18:07:48 +0400

    Added NED logo to footer.

--- src/components/Layout/components/Footer.jsx
+++ src/components/Layout/components/Footer.jsx
@@ -1,4 +1,6 @@
 const React = require('react')
+const Svg = require('src/components/shared/Svg')
+const nedLogo = require('public/images/ned_logo')
 
 const Footer = () => (
   <div className='gb-Footer'>
@@ -7,9 +9,15 @@ const Footer = () => (
         <span className='gb-Footer-content-copyright'>© </span><span>{(new Date()).getFullYear()}</span>
       </div>
 
-      <a href='https://jumpstart.ge/' target='blank'>
-        <img src='/public/images/jumpstart_logo.png' alt="JumpStart's Logo" />
-      </a>
+      <div className='gb-Footer-content-right'>
+        <a className='gb-Footer-content-right-ned' href='http://www.ned.org/' target='blank' title="National Endowment for Democracy">
+          <Svg markup={nedLogo} />
+        </a>
+
+        <a href='https://jumpstart.ge/' target='blank'>
+          <img src='/public/images/jumpstart_logo.png' alt="JumpStart's Logo" />
+        </a>
+      </div>
     </footer>
   </div>
 )
