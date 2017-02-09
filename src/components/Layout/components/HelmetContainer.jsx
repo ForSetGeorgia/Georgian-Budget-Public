99b72966d41877b5d53f5663e111611c7f2e21cc
nwshane <django09@gmail.com>
2017-02-09 18:22:51 +0400

    Translated app name in title tag.

--- src/components/Layout/components/HelmetContainer.jsx
+++ src/components/Layout/components/HelmetContainer.jsx
@@ -1,17 +1,21 @@
 const React = require('react')
 const { string } = React.PropTypes
+const { injectIntl, intlShape } = require('react-intl')
 const Helmet = require('react-helmet')
+
+const appMessages = require('src/messages/app')
 
 const HelmetContainer = props => (
   <Helmet
     htmlAttributes={{'lang': props.locale}}
     title='Home'
-    titleTemplate='%s | Georgian Budget'
+    titleTemplate={`%s | ${props.intl.formatMessage(appMessages.name)}`}
   />
 )
 
 HelmetContainer.propTypes = {
-  locale: string.isRequired
+  locale: string.isRequired,
+  intl: intlShape.isRequired
 }
 
-module.exports = HelmetContainer
+module.exports = injectIntl(HelmetContainer)
---
+++ src/messages/app.js
@@ -0,0 +1,8 @@
+const { defineMessages } = require('react-intl')
+
+module.exports = defineMessages({
+  name: {
+    id: 'app.name',
+    defaultMessage: 'Budget Stories from Georgia'
+  }
+})
