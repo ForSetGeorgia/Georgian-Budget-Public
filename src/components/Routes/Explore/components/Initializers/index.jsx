20fab5e4a3570de02033ab5eea75f4d1ea98ead7
nwshane <django09@gmail.com>
2017-02-20 10:36:02 +0400

    Changed order of initializers.

--- src/components/Routes/Explore/components/Initializers/index.jsx
+++ src/components/Routes/Explore/components/Initializers/index.jsx
@@ -1,18 +1,18 @@
 const React = require('react')
 
+const DetailsItemIdInitializer = require('./components/DetailsItemIdInitializer')
 const BudgetItemTypeInitializer = require('./components/BudgetItemTypeInitializer')
 const FinanceTypeInitializer = require('./components/FinanceTypeInitializer')
 const TimePeriodsInitializer = require('./components/TimePeriodsInitializer')
 const TimePeriodTypeInitializer = require('./components/TimePeriodTypeInitializer')
-const DetailsItemIdInitializer = require('./components/DetailsItemIdInitializer')
 
 const Initializers = () => (
   <div>
+    <DetailsItemIdInitializer />
     <BudgetItemTypeInitializer />
     <FinanceTypeInitializer />
     <TimePeriodsInitializer />
     <TimePeriodTypeInitializer />
-    <DetailsItemIdInitializer />
   </div>
 )
 
