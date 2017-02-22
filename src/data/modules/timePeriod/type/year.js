d338ba8c939d2321588eaf3a87032af41e3911be
nwshane <django09@gmail.com>
2017-02-22 15:54:37 +0400

    Reversed order of years.

--- src/data/modules/timePeriod/type/year.js
+++ src/data/modules/timePeriod/type/year.js
@@ -2,7 +2,7 @@ const { getSelectedTimePeriods } = require('src/data/ducks/filters')
 const Year = {}
 
 Year.getYearsWithData = () => (
-  ['y2016', 'y2015', 'y2014', 'y2013', 'y2012']
+  ['y2012', 'y2013', 'y2014', 'y2015', 'y2016']
 )
 
 Year.getSelectedYears = state => {
