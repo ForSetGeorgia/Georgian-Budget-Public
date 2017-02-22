5301bb682e5b2f6d88fc52e6c2d5a96a861b4a9f
nwshane <django09@gmail.com>
2017-02-22 17:59:40 +0400

    Added translations for next and previous pagination controls.

--- src/components/shared/CustomGriddle.jsx
+++ src/components/shared/CustomGriddle.jsx
@@ -1,18 +1,33 @@
 const React = require('react')
+const { defineMessages, injectIntl, intlShape } = require('react-intl')
 const { string } = React.PropTypes
 const Griddle = require('griddle-react')
+
+const messages = defineMessages({
+  next: {
+    id: 'app.table.next',
+    defaultMessage: 'Next'
+  },
+  previous: {
+    id: 'app.table.previous',
+    defaultMessage: 'Previous'
+  }
+})
 
 const CustomGriddle = props => (
   <Griddle
     rowMetadata={{ bodyCssClassName: 'gb-Table-row mod-darker-if-odd' }}
     useGriddleStyles={false}
     resultsPerPage={10}
+    nextText={props.intl.formatMessage(messages.next)}
+    previousText={props.intl.formatMessage(messages.previous)}
     {...props}
   />
 )
 
 CustomGriddle.propTypes = {
-  modTableClassName: string
+  modTableClassName: string,
+  intl: intlShape
 }
 
-module.exports = CustomGriddle
+module.exports = injectIntl(CustomGriddle)
