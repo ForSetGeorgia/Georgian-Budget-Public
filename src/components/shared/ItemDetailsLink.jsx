094258c719e388db70bb19db2824c213a71b5903
nwshane <django09@gmail.com>
2017-02-24 12:03:29 +0400

    Only showing item details link if name is present.

--- src/components/shared/ItemDetailsLink.jsx
+++ src/components/shared/ItemDetailsLink.jsx
@@ -38,6 +38,8 @@ const ItemDetailsLink = React.createClass({
   },
 
   render () {
+    if (!this.props.name) return null
+    
     return (
       <Link className='gb-Link' onClick={this.navigateToBudgetItem}>
         ← {this.text()}
